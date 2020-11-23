03f559714fbaa90ac9da788b5b3cc7807ce59731
imasandrianfajri <64575394+rian1699@users.noreply.github.com>
2020-11-23 08:34:06 +0700

    changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function getInput(){
+    var a=document.forms["biodata"]["nama"].value;
+    var b=document.forms["biodata"]["alamat"].value;
+    var c=document.forms["biodata"]["e-mail"].value;
+    var d=document.forms["biodata"]["username"].value;
+    var e=document.forms["biodata"]["telp"].value;
+
+    var tabel = document.getElementById("tabelinput");
+}
