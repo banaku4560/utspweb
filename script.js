5c1c39241d247ec85cbfaec25a81b93d4f41d885
imasandrianfajri <64575394+rian1699@users.noreply.github.com>
2021-01-14 20:33:58 +0700

    javascript login

--- script.js
+++ script.js
@@ -7,3 +7,49 @@ function getInput(){
 
     var tabel = document.getElementById("tabelinput");
 }
+
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == null || username == "") {
+        alert("Silahkan masukkan username.")
+        return false;
+    }
+    if (password == null || password == "") {
+        alert("Silahkan masukkan password.");
+        return false;
+    }
+    alert('Login berhasil');
+    window.location = "\\beranda-logged-in.html";
+}
+
+// function validate() {
+//     var x = document.forms["myForm"]["fname"].value;
+//     if (x == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     }
+// }
+
+// var attempt = 3; // Variable to count number of attempts.
+// // Below function Executes on click of login button.
+// function validate(){
+// var username = document.getElementById("username").value;
+// var password = document.getElementById("password").value;
+// if ( username == "bagusnayoko" && password == "bagus12345"){
+// alert ("Login successfully");
+// window.location = "beranda-logged-in.html"; // Redirecting to other page.
+// return false;
+// }
+// else{
+// attempt --;// Decrementing by one.
+// alert("You have left "+attempt+" attempt;");
+// // Disabling fields after 3 attempts.
+// if( attempt == 0){
+// document.getElementById("username").disabled = true;
+// document.getElementById("password").disabled = true;
+// document.getElementById("submit").disabled = true;
+// return false;
+// }
+// }
+// }
