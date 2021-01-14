976ceb8b7eb779d2a5bf74f7bed52c57dee1989b
imasandrianfajri <64575394+rian1699@users.noreply.github.com>
2021-01-14 20:50:06 +0700

    revisi login js

--- script.js
+++ script.js
@@ -22,34 +22,3 @@ function validate() {
     alert('Login berhasil');
     window.location = "\\beranda-logged-in.html";
 }
-
-// function validate() {
-//     var x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-// }
-
-// var attempt = 3; // Variable to count number of attempts.
-// // Below function Executes on click of login button.
-// function validate(){
-// var username = document.getElementById("username").value;
-// var password = document.getElementById("password").value;
-// if ( username == "bagusnayoko" && password == "bagus12345"){
-// alert ("Login successfully");
-// window.location = "beranda-logged-in.html"; // Redirecting to other page.
-// return false;
-// }
-// else{
-// attempt --;// Decrementing by one.
-// alert("You have left "+attempt+" attempt;");
-// // Disabling fields after 3 attempts.
-// if( attempt == 0){
-// document.getElementById("username").disabled = true;
-// document.getElementById("password").disabled = true;
-// document.getElementById("submit").disabled = true;
-// return false;
-// }
-// }
-// }
